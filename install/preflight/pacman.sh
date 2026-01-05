2011343d5964871cd61d6fd6531262871cfc386e
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2026-01-06 00:02:25 +0200

    add pacman conf

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -2,7 +2,7 @@
 sudo pacman -S --needed --noconfirm base-devel
 
 # Configure pacman
-sudo cp -f ~/.local/share/shogun/default/pacman/pacman.conf /etc/pacman.conf
+sudo cp -f ~/.local/share/shogun/default/pacman.conf /etc/pacman.conf
 
 # Refresh all repos
 sudo pacman -Syu --noconfirm
